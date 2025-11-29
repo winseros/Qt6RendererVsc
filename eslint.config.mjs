53ee73c5bdd9159976fef68f793a4cd6798da671
Nikita Kobzev <win.accuracy@gmail.com>
2025-11-30 00:30:06 +0300

    Update dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,18 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
 
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.strictTypeChecked, {    
-    rules: {
-        "sort-imports": ["error"]
-    },
-    languageOptions: {
-        parserOptions: {
-            projectService: true,
-            tsconfigRootDir: import.meta.dirname,
-            sourceType: "module"
+export default defineConfig([
+    eslint.configs.recommended,
+    tseslint.configs.strictTypeChecked,
+    {
+        rules: { "sort-imports": ["error"] },
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+                sourceType: "module"
+            }
         }
     }
-});
+]);
